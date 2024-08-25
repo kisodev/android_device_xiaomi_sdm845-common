ce4ebc2afbdb422b6ddd51077fd437d7af2ee8ce
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 21:46:46 +0300

    sdm845-common: Patch libwfdservice to resolve duplicate dependencies
    Change-Id: I8086a80fcc238d6b03d16640ee2c4e134129bbab
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+   'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so','android.media.audio.common.types-V4-cpp.so'),
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
