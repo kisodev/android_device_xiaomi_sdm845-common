d5a3a564d9c17db5b7577ee804215bb9bf37c7c9
Marc Bourgoin <themard85@gmail.com>
2024-11-21 13:24:16 -0700

    sdm845-common: Remove copy libutils-v33
    With enforced elf checks coming on A16, let's switch to the provided one from compat
    
    Change-Id: I193682ca769e902b7a8b2edd850ea14297a71dec

--- sdm845.mk
+++ sdm845.mk
@@ -386,7 +386,9 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
+
+PRODUCT_PACKAGES += \
+    libutils-v33
 
 # WiFi
 PRODUCT_PACKAGES += \
