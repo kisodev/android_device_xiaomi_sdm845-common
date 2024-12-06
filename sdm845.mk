31ea73eaf09505b2b8e9a384aec98bef57a6f29f
neroices <git@slce.moe>
2024-12-07 08:50:01 +0900

    Revert "sdm845-common: Build android.frameworks.sensorservice@1.0"
    This reverts commit b0127c817f986dbaa60fd4a6cff8d117508cd94a.
    
    Change-Id: If967623a6af609528bc77f94e514c9272b6cf70c

--- sdm845.mk
+++ sdm845.mk
@@ -327,7 +327,6 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
