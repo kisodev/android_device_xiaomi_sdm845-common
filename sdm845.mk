894619e05bfae2ef3fbce0be87794213a0d3a1ad
duckyduckG <duckyduckg65@gmail.com>
2025-03-01 00:29:30 +0500

    Revert "sdm845-common: Retrofit dynamic partitions"
    This reverts commit 3dbfb35d465d225d921507e110b1bb7dada70f94.

--- sdm845.mk
+++ sdm845.mk
@@ -289,9 +289,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
-# Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
