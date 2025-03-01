60053b5983f8e01e1828f4c4351b2f5d23564646
Vhmit <viktor.9630@protonmail.com>
2025-03-01 17:13:19 +0500

    sdm845-common: vintf: Added missing entries to FCM & manifest I servicemanager: Could not find android.hardware.radio.data.IRadioData/slot1 in the VINTF manifest. I servicemanager: Could not find android.hardware.radio.messaging.IRadioMessaging/slot1 in the VINTF manifest. I servicemanager: Could not find android.hardware.radio.modem.IRadioModem/slot1 in the VINTF manifest. I servicemanager: Could not find android.hardware.radio.network.IRadioNetwork/slot1 in the VINTF manifest. I servicemanager: Could not find android.hardware.radio.sim.IRadioSim/slot1 in the VINTF manifest. I servicemanager: Could not find android.hardware.radio.voice.IRadioVoice/slot1 in the VINTF manifest. I servicemanager: Could not find android.hardware.radio.modem.IRadioModem/slot1 in the VINTF manifest. I hwservicemanager: getTransport: Cannot find entry android.hardware.radio@1.6::IRadio/slot1 in either framework or device VINTF manifest.
    I hwservicemanager: getTransport: Cannot find entry android.hardware.radio@1.6::IRadio/slot1 in either framework or device VINTF manifest.
    I hwservicemanager: getTransport: Cannot find entry android.hardware.radio@1.6::IRadio/slot2 in either framework or device VINTF manifest.
    I hwservicemanager: getTransport: Cannot find entry android.hardware.radio.config@1.3::IRadioConfig/default in either framework or device VINTF manifest.
    
    * FCM: declare required versions
    * Manifest:
    [1] uprev android.hardware.radio.config to 1.2 not 1.3
    [2] uprev android.hardware.radio to 1.6
    
    * reason: uprev android.hardware.radio to 1.6 & android.hardware.radio to 1.3 -> radio services(sim) not working
    
    Change-Id: I05da2ca9495e344d43feb7eb869fbd1fe2548a78
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,7 +72,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := \
+    hardware/qcom-caf/common/compatibility_matrix.xml \
+    $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 
 # LMKD
 TARGET_LMKD_STATS_LOG := true
