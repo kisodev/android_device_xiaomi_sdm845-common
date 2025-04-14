33dd5a9cf6d6cac1fc5b4414eada843d50eb8eb3
duckyduckg <duckyduckg65@gmail.com>
2025-04-15 04:58:27 +0500

    BoardConfigCommon: update kernel configs path
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,8 +44,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := \
     vendor/sdm845-perf_defconfig \
+    vendor/xiaomi/sdm845-common.config \
     vendor/xiaomi/beryllium.config \
-    vendor/feature/full_lto.config
+    vendor/extras/full-lto.config
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
