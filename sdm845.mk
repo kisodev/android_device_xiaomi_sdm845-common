ca5d7474266edaef1f05db15db8fc41920cd4449
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2025-05-09 22:27:39 +0500

    sdm845-common: Enable freeform windows Signed-off-by: Pranav Vashi <neobuddy89@gmail.com> Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -172,6 +172,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
