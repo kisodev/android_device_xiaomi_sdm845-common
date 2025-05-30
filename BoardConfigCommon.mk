5e55f76d251087a4a291e9c861967565a9f6827d
duckyduckg <duckyduckg65@gmail.com>
2025-05-30 08:02:01 +0500

    sdm845: update kernel config path
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,6 +46,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := \
     vendor/sdm845-perf_defconfig \
     vendor/xiaomi/sdm845-common.config \
+    vendor/xiaomi/dipper.config \
     vendor/extras/full-lto.config
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
