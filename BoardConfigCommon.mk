5babddfcf625b52654bdd3aac68692481ee02901
duckyduckg <duckyduckg65@gmail.com>
2025-06-06 02:21:22 +0500

    sdm845-common: BoardConfig: remove beryllium config * not needed anymore
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := \
     vendor/sdm845-perf_defconfig \
     vendor/xiaomi/sdm845-common.config \
-    vendor/xiaomi/beryllium.config \
     vendor/extras/full-lto.config
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
